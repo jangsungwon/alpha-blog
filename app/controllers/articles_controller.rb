edc071b0dd24a4d89dd11c0209a6b25f059a24cc
jangsungwon <ssw024@gmail.com>
2020-11-16 14:43:30 +0900

    -

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    byebug
       @article = Article.new(article_params)
       @article.user = current_user
       #render plain: @article.inspect
